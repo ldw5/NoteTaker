87531f8a33f63f67f8106fdaecd11e4d4c3d1efd
Landrell Williams <dontess5@yahoo.com>
2021-11-14 14:06:39 -0500

    updated server and html js, still need to do api js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-// start app then make a port
-// set up middleware
+const express = require("express");
+const htmlR = require("./routes/html");
+const apiR = require("./routes/api");
+// start app with a port
+const PORT = process.env.PORT || 3001;
+const app = express();
+// setting up route middleware connections
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use("/api",apiR);
+app.use("/",htmlR);
 // start the server
+app.listen(PORT,()=> console.log(`I'm listening on PORT: ${PORT}`));
