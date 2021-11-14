07fa78b45ee41681e5913ccee343cec90ea14326
Landrell Williams <dontess5@yahoo.com>
2021-11-14 14:38:49 -0500

    updated api route file

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,26 @@
 const routes = require("express").Router();
 const storage = require("/notes");
-//get request from db
+//get notes request from db
+//req response from http with info, then res to send back http response
+routes.get("/notes",(req,res) => {
+    storage
+    .getNotes().then((notes) => {
+        return res.json(notes);
+    }).catch((err)=> res.status(500).json(err));
+});
 //post request for notes to db
+//req response from http with info, then res to send back http response
+routes.get("/notes",(req,res) => {
+    storage
+    .addNotes(req.body).then((notes) => res.json(notes))
+    .catch((err)=> res.status(500).json(err));
+});
+
 //delete request to remove notes from db
+//req response from http with info, then res to send back http response
+routes.delete("/notes/:id",(req,res) => {
+    storage
+    .removeNotes(req.params.id).then(() => res.json({ok:true}))
+    .catch((err)=> res.status(500).json(err));
+});
+module.exports = routes;
